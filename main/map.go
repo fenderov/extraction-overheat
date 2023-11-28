270b39f29c9b9d3d28e73e51ded5c1f8800ace40
Fenderov Andrey <dreosashes@gmail.com>
2023-11-28 23:26:56 +0300

    simple map

---
+++ main/map.go
@@ -0,0 +1,30 @@
+components {
+  id: "hex_factory"
+  component: "/main/hex_factory.factory"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "map"
+  component: "/main/map.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
